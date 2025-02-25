7b59741729395eb89c51bc6170edf68d39c050c1
Matthew Flynn <90197912+mpflynnx@users.noreply.github.com>
2025-02-25 09:21:03 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix Flake for an xfce-based system with YubiKey setup";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     drduhConfig.url = "github:drduh/config";
     drduhConfig.flake = false;
   };
